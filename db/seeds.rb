638f222201ed639de8e0176d9276a845396bba40
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-08 10:14:50 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning DB"
+Restautant.destroy_all
+puts "creating DB"
+resto1 = { name: "la tour d'argent", address: "avenue charles de gaule", stars: "5"}
+resto2 = { name: "chez Wung", address: "derriere la muraille ", stars: "3"}
+resto3 = { name: "Boubou sauvage", address: "1 alleee de la cité", stars: "2"}
+resto4 = { name: "mcdonald", address: "avenue de clichy", stars: "0"}
+resto5 = { name: "ritz", address: "avenue charles de gaule", stars: "5"}
+Restaurants = []
+[resto1, resto2, resto3, resto4, resto5].each_with_index do |attributes,index|
+restaurant = Restautant.create!(attributes)
+Restautants << restaurant
+end

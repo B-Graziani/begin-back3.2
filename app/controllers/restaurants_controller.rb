8534570f78094dfe021ec25daed34cb6631d5033
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-08 11:13:38 +0200

    ad top routes & fonct

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,31 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def top
+    @restaurants = Restaurant.where(stars:5)
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
+  end
 end

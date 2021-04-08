c856086a26bc5cc5576fe3bf16a0493d8c6728d6
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-08 17:00:51 +0200

    retoour

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -23,6 +23,11 @@ class RestaurantsController < ApplicationController
   def edit
     @restaurant = Restaurant.find(params[:id])
   end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
